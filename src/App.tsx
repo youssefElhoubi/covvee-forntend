dc01feb6193948a786a0c44df8e809398edb9465
youssefElhoubi <elhoubiyoussef@gmail.com>
2026-03-04 15:39:31 +0000

    feat: add Footer component and update LandingPage to include it

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
+import LandingPage from './pages/LandingPage'
 
 function App() {
 
   return (
     <>
+    <BrowserRouter>
+        <Routes>
+          {/* <Route path="*" element={<PageNotFound/>} /> */}
+          {/* <Route path="/" element={<Login/>} /> */}
+          <Route path="/" element={<LandingPage/>} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
