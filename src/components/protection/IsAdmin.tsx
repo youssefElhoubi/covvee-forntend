91d99c8d7a749221cbf0bb63817b4cc83968e324
youssefElhoubi <elhoubiyoussef@gmail.com>
2026-03-31 15:25:13 +0100

    refactor: update IsAdmin component to use token for user authentication and improve loading state handling

--- src/components/protection/IsAdmin.tsx
+++ src/components/protection/IsAdmin.tsx
@@ -3,31 +3,34 @@ import type { IsLogedProps } from './IsLoged'
 import { useNavigate } from 'react-router-dom';
 import { jwtDecode } from 'jwt-decode';
 
-
 const IsAdmin: React.FC<IsLogedProps> = ({ children }) => {
     const [isAdmin, setIsAdmin] = useState(false);
     const [isLoading, setIsLoading] = useState(true);
-    const navigator = useNavigate()
+    const navigate = useNavigate();
+
     useEffect(() => {
-        const user = JSON.parse(localStorage.getItem('user') || 'null');
-        const decodedUser = jwtDecode<{ roles?: string[] }>(user);
-        if (decodedUser?.roles?.includes('ROLE_ADMIN')) {
-            setIsAdmin(true);
-        } else {
-            navigator('/login');
+        try {
+            const user = localStorage.getItem('token') || 'null';
+            if (!user || user === 'null') {
+                navigate('/login');
+            }
+            const decodedUser = jwtDecode<{ roles?: string[] }>(user);
+            if (decodedUser?.roles?.includes('ROLE_ADMIN')) {
+                setIsAdmin(true);
+            } else {
+                navigate('/login');
+            }
+        } catch (error) {
+            navigate('/login');
+        } finally {
+            setIsLoading(false);
         }
-        setIsLoading(false);
-    }, [navigator])
-
+    }, [navigate]);
+    
     if (isLoading) {
-        return <div>Loading...</div>
+        return <div>Loading...</div>;
     }
+    return <>{isAdmin && children}</>;
+};
 
-    return (
-        <div>
-            {isAdmin && children }
-        </div>
-    )
-}
-
-export default IsAdmin
+export default IsAdmin;
